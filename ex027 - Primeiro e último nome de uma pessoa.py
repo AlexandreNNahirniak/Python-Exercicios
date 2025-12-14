706a125a6557155d8f80fe308fe18c6c8614566d
Alexandre Das Neves Nahirniak <alexandrenahirniak.curriculo@gmail.com>
2025-12-13 23:36:46 -0300

    att 01

---
+++ ex027 - Primeiro e último nome de uma pessoa.py
@@ -0,0 +1,5 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+nome = nome.split()
+print('Muito prazer em te conhecer')
+print(f'Seu primeiro nome é {nome[0]}')
+print(f'Seu último nomoe é {nome[-1]}')
