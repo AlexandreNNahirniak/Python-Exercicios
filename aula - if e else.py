90db59e1f47d8e951f09667b04a0cd4986f4bd94
Alexandre Das Neves Nahirniak <alexandrenahirniak.curriculo@gmail.com>
2025-12-26 12:20:54 -0300

    acrescentado mais testes

--- aula - if e else.py
+++ aula - if e else.py
@@ -10,10 +10,19 @@ tempo = int(input('Quantos anos tem seu carro ? '))
 print('Carro novo' if tempo <=3 else 'Carro antigo')
 print('--FIM--')'''
 
-#Outro teste
+'''#Outro teste
 nome = str(input('Qual o seu nome ? '))
 if nome == 'Alexandre':
     print(f'Seja bem-vindo de volta {nome}')
 else:
     print(f'Olá, seja bem-vindo {nome}')
-print('Bom dia!')
+print('Bom dia!')'''
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m = (n1+n2) / 2
+print(f'A sua média de notas foi {m}')
+if m >= 6:
+    print('Aprovado, Parabéns')
+else:
+    print('Reprovado, estude mais!')
