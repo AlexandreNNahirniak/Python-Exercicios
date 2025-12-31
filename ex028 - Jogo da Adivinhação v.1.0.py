2f1b629672f793cab99f6a967590396d0956983b
Alexandre Das Neves Nahirniak <alexandrenahirniak.curriculo@gmail.com>
2025-12-31 12:10:12 -0300

    arquivo novo ex028

---
+++ ex028 - Jogo da Adivinhação v.1.0.py
@@ -0,0 +1,13 @@
+from random import randint
+from time import sleep
+com = randint(0, 5) # Faz o computador "pensar"
+print('-=-' * 20)
+print ('Vou pensar em um número entre 0 a 5, tente adivinhar...')
+print('-=-' * 20)
+jogador = int(input('Em que número eu pensei ? ')) # Jogador tenta adivinhar
+print('PROCESSANDO...')
+sleep(2)
+if jogador == com:
+    print('Parabéns, você ganhou!!!')
+else:
+    print(f'Eu ganhei, eu pensei no número {com} e não no número {jogador}!')
