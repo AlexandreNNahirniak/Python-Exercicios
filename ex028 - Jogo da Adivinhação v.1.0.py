663c291507d0cedd09b1b6cc4f7c58544c306e6b
Alexandre Das Neves Nahirniak <alexandrenahirniak.curriculo@gmail.com>
2026-01-02 22:16:04 -0300

    att nome da variavel

--- ex028 - Jogo da Adivinhação v.1.0.py
+++ ex028 - Jogo da Adivinhação v.1.0.py
@@ -1,13 +1,13 @@
 from random import randint
 from time import sleep
-com = randint(0, 5) # Faz o computador "pensar"
+cpu = randint(0, 5) # Faz o computador "pensar"
 print('-=-' * 20)
 print ('Vou pensar em um número entre 0 a 5, tente adivinhar...')
 print('-=-' * 20)
 jogador = int(input('Em que número eu pensei ? ')) # Jogador tenta adivinhar
 print('PROCESSANDO...')
 sleep(2)
-if jogador == com:
+if jogador == cpu:
     print('Parabéns, você ganhou!!!')
 else:
-    print(f'Eu ganhei, eu pensei no número {com} e não no número {jogador}!')
+    print(f'Eu ganhei, eu pensei no número {cpu} e não no número {jogador}!')
